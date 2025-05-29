7fb6406b2c4c0f388e527abf4646fed967b16f89
tatyana-prokopenko <tatyana-prokopenko@bk.ru>
2025-05-29 21:01:56 +0300

    feat: make towelSort function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const newArray = [];
+  if (Array.isArray(matrix)) {
+    for (let i = 0; i < matrix.length; i += 1) {
+      if (Array.isArray(matrix[i])) {
+        if ((i + 2) % 2 === 0) {
+          newArray.push(...matrix[i]);
+        } else {
+          newArray.push(...matrix[i].reverse());
+        }
+      }
+    }
+  }
+  return newArray;
 };
